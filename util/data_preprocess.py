a0cc1fcea1404b5eff24f5a23491f6cc4ec70863
zipeijiang <60901774+zipeijiang@users.noreply.github.com>
2022-12-05 15:03:40 -0500

    update

--- util/data_preprocess.py
+++ util/data_preprocess.py
@@ -1,4 +1,3 @@
-import os
 import json
 import pickle
 import numpy as np
@@ -6,43 +5,6 @@ import pandas as pd
 import tqdm
 from scipy.sparse import dok_matrix
 from sklearn.preprocessing import StandardScaler
-
-FILES = os.listdir("C:/Users/14809/Desktop/W4995AML/Final-Project/data")
-FILES.sort(key = lambda x: int(x.split('.')[2].split('-')[0]))
-
-# not used
-def load_data_to_df(num_files):
-    
-    track_uris = dict() # store all unique uris
-    playlists_raw = [] # store uris and other info of each 
-    tracks_raw = []
-    
-    files_to_read = FILES[:num_files]
-    for file in tqdm.tqdm(files_to_read):
-        data = json.load(open("C:/Users/14809/Desktop/W4995AML/Final-Project/data/"+file))
-        playlists = data['playlists']  # 1k playlists
-                
-        for playlist in playlists:  
-            tracks = playlist['tracks']
-            for track in tracks:
-                track_uri = track['track_uri'] # uri uniquely identifies a track
-                if track_uri not in track_uris:
-                    index = len(track_uris)
-                    track_uris[track_uri] = index
-                    tracks_raw.append(track)
-                    #print(track_uris[track_uri] )
-                track['track_uri'] = track_uris[track_uri] 
-                
-            playlist['tracks'] = [track['track_uri'] for track in tracks] # only want the uri
-            playlists_raw.append(playlist)
-        
-    df_playlists = pd.DataFrame(playlists_raw)
-    df_playlists.set_index('pid')
-        
-    df_tracks = pd.DataFrame(tracks_raw)
-    df_tracks.set_index('track_uri')
-
-    return df_playlists, df_tracks
 
 def read_pickle(path_o, name_in):
     import pickle
@@ -87,7 +49,6 @@ def create_audio_matrix(playlists, tracks):
     return np.array(matrix)
 
 def preprocess_and_save_data(num_to_read):
-    #df_playlists, df_tracks = load_data_to_df(num_files)
     df_playlists = read_pickle("C:/Users/14809/Desktop/W4995AML/Final-Project/lib/", "playlist")[:num_to_read]
     df_tracks = read_pickle("C:/Users/14809/Desktop/W4995AML/Final-Project/lib/", "tracks_df")
     #sparse_matrix = create_sparse_matrix(df_playlists, df_tracks)
